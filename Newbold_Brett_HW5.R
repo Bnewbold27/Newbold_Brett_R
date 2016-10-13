2934d3242c8d7ea2c85072ec83c1a17203f2a979
Bnewbold27 <bcn13@georgetown.edu>
2016-10-13 11:38:59 -0400

    HW 5

--- Newbold_Brett_HW5.R
+++ Newbold_Brett_HW5.R
@@ -2,66 +2,46 @@
   HW5 
 ---
   
-<<<<<<< HEAD
 ---
   # 1. Print to the console all methods and attributes associated with a dataframe. 
   # Write code to determine the number of columns in a dataframe
 ---
-
+  
 require(ggplot2)
-methods(class = "data.frame")
-
-# Only wanted to print the first ten row names so we can see column names on the 
-# console as part of the attritbutes
-attributes(diamonds[1:10,])
-ncol(diamonds)
-
----
-  # 2. Write code to determine how many rows are in a dataframe
---- 
-  
-nrow(diamonds)
-=======
-  ---
-  # 1. Print to the console all methods and attributes associated with a dataframe. 
-  # Write code to determine the number of columns in a dataframe
-  ---
-  
-  require(ggplot2)
 
 Df_Info <- function(x)
-  #  This function Df_Info() accepts any dataframe as a parameter
+    #  This function Df_Info() accepts any dataframe as a parameter
   
-  #  Parameters: 
-  #  x - dataFrame
+    #  Parameters: 
+    #  x - dataFrame
   
-  #  Returns:
-  #  all methods and attributes of the data frame 
+    #  Returns:
+    #  all methods and attributes of the data frame 
   
-  #  bn-2016
-{
-  methods(class = x)   # Will be the same for any data frame
-  attributes(x)
-}
+    #  bn-2016
+  {
+    methods(class = x)   # Will be the same for any data frame
+    attributes(x)
+  }
 
 data("diamonds")
-Df_Info(diamonds[1:10,]) # Only wanted to print the first ten row names so we can see column names on the
-# console as part of the attritbutes.
+Df_Info(diamonds[1:10,])  # Only wanted to print the first ten row names so we can see column names on the
+                          # console as part of the attritbutes.
 
 
 Column_Num <- function(x)
-  #  This function Column_Num() accepts any dataframe as a parameter
+    #  This function Column_Num() accepts any dataframe as a parameter
   
-  #  Parameters: 
-  #  x - dataFrame
+    #  Parameters: 
+    #  x - dataFrame
   
-  #  Returns:
-  #  the number of columns in a dataFrame 
+    #  Returns:
+    #  the number of columns in a dataFrame 
   
-  #  bn-2016
-{
-  ncol(x)
-}
+    #  bn-2016
+  {
+    ncol(x)
+  }
 
 data("diamonds")
 Column_Num(diamonds)
@@ -69,9 +49,9 @@ Column_Num(diamonds)
 
 ---
   # 2. Write code to determine how many rows are in a dataframe
-  --- 
+--- 
   
-  Row_Num <- function(x)
+Row_Num <- function(x)
     #  This function Row_Num() accepts any dataframe as a parameter
     
     #  Parameters: 
@@ -87,19 +67,13 @@ Column_Num(diamonds)
 
 data("diamonds")
 Row_Num(diamonds)
->>>>>>> my-feature-branch
 
 ---
   # 3. Write code to extract the column names from a dataframe and print the
   # names of the columns (one per line) to the console.
-<<<<<<< HEAD
 ---
-
-cat(colnames(diamonds), sep = "\n")
-=======
-  ---
   
-  Column_Names <- function(x)
+Column_Names <- function(x)
     #  This function Column_Names() accepts any dataframe as a parameter
     
     #  Parameters: 
@@ -115,19 +89,13 @@ cat(colnames(diamonds), sep = "\n")
 
 data("diamonds")
 Column_Names(diamonds)
->>>>>>> my-feature-branch
 
 ---
   # 4. Write code to determine the type of each column (numeric, factor, logical,
   # etc.). Print the type of each column to the console.
-<<<<<<< HEAD
 ---
   
-lapply(diamonds, class)
-=======
-  ---
-  
-  Column_Type <- function(x)
+Column_Type <- function(x)
     #  This function Column_Type() accepts any dataframe as a parameter
     
     #  Parameters: 
@@ -143,33 +111,13 @@ lapply(diamonds, class)
 
 data("diamonds")
 Column_Type(diamonds)
->>>>>>> my-feature-branch
 
 ---
   # 5. Write code that will loop through any dataframe and calculate the mean of
   # every numeric column. Label the output with the name of the column.
-<<<<<<< HEAD
 ---
-
-# sapply() uses "is.numeric" to find the columns of diamonds that are numeric, 
-# colMeans() forms the mean of each numeric column found in diamonds
-numeric_cols <- diamonds[sapply(diamonds, is.numeric)]
-colMeans(numeric_cols)
   
----
-  # 6. Write code that will loop through any dataframe and create a frequency table
-  # for every factor column. Label the output with the name of the column.
----
-
-# sapply() will reveal which columns in diamonds are factor columns. diamonds[] 
-# outputs those column that are TRUE. summary() will create a frequency table 
-# because factors are being passed through
-fctr_cols <- diamonds[sapply(diamonds, is.factor)]
-summary(fctr_cols) 
-=======
-  ---
-  
-  Column_Means <- function(x)
+Column_Means <- function(x)
     #  This function Column_Means() accepts any dataframe as a parameter
     
     #  Parameters: 
@@ -180,8 +128,8 @@ summary(fctr_cols)
     
     #  bn-2016
   {
-    numeric_cols <- x[sapply(x, is.numeric)]   # sapply() uses "is.numeric" to find the columns of diamonds that are numeric
-    colMeans(numeric_cols)    # colMeans() forms the mean of each numeric column found in diamonds
+    numeric_cols <- x[sapply(x, is.numeric)]   # sapply() uses "is.numeric" to find the columns of dataFrame that are numeric
+    colMeans(numeric_cols)    # colMeans() forms the mean of each numeric column found in dataFrame
   }
 
 data("diamonds")
@@ -190,9 +138,9 @@ Column_Means(diamonds)
 ---
   # 6. Write code that will loop through any dataframe and create a frequency table
   # for every factor column. Label the output with the name of the column.
-  ---
+---
   
-  Freq_Table <- function(x)
+Freq_Table <- function(x)
     #  This function Freq_Table() accepts any dataframe as a parameter
     
     #  Parameters: 
@@ -203,44 +151,21 @@ Column_Means(diamonds)
     
     #  bn-2016
   {
-    fctr_cols <- x[sapply(x, is.factor)]   # sapply() will reveal which columns in x are factor columns. 
-    # x[] outputs those column that are TRUE. 
+    fctr_cols <- x[sapply(x, is.factor)]   #  sapply() will reveal which columns in x are factor columns. 
+                                           #  x[] outputs those column that are TRUE. 
     summary(fctr_cols)   # summary() will create a frequency table because factors are being passed through
   }
 
 data("diamonds")
 Freq_Table(diamonds)
->>>>>>> my-feature-branch
 
 ---
   # 7. Write code that will loop through any dataframe and determine the number of 
   # rows containing NA (missing value) in EACH column and the percentage of rows
   # containing an NA in ANY of the columns
-<<<<<<< HEAD
----
-
-# is.na() will create logical vectors for each entry in the dataframe, rowSums 
-# will reveal which rows have an equal amount of missing values as they do 
-# columns. sum() will then add the TRUEs from rowSums
-sum(rowSums(is.na(diamonds))==ncol(diamonds))
-
-# To find the number of rows containing an NA in any of the columns I took nrow()
-# (total number of rows) and subtracted nrow(na.omit()) (the amount of rows 
-# without NAs) 
-N <- nrow(diamonds) - nrow(na.omit(diamonds))
-N
-
-# paste() works well to concatenate
-paste(100*(N / nrow(diamonds)), "%")
-
-# diamonds was a bad dataFrame to test with #7 considering 
-sum(is.na(diamonds))
-# there are no NA values in diamonds. To test I created the following dateFrame:
-
-=======
   ---
   
-  NA_summary <- function(x)
+NA_summary <- function(x)
     #  This function NA_Summary() accepts any dataframe as a parameter
     
     #  Parameters: 
@@ -269,84 +194,24 @@ sum(is.na(diamonds))   # diamonds was a bad dataFrame to test with #7 considerin
 
 
 #To test I created the following dataFrame:
->>>>>>> my-feature-branch
 a <- c(NA,40,72,NA)
 b <- c(NA, NA, 36, NA)
 c <- c(NA,4,5,NA)
 d <- c(NA,5,4,NA)
 A <- data.frame(a,b,c,d)
 
-<<<<<<< HEAD
-print(A)
-sum(is.na(A))
-
-sum(rowSums(is.na(A))==ncol(A))
-
-N <- nrow(A) - nrow(na.omit(A))
-N
-paste(100*(N / nrow(A)), "% of the rows contain NA in at least one column")
-=======
 print(A) 
 
 NA_summary(A)
->>>>>>> my-feature-branch
 
 
 ---
   # 8. Create an R function that can accept any dataframe as a parameter and
   # returns a dataframe that contains each pair of column names and their 
   # corresponding Pearson correlation coefficient
-<<<<<<< HEAD
 ---
-
-corCoef <- function(x){
-#  This function corCoef() accepts any dataframe as a parameter and returns a
-#  dataframe that contains each pair of column names in the first column as a 
-#  single string separated by a "-" and their corresponding Pearson correlation
-#  coefficient in the second column.
   
-#  Parameters: 
-#  x - dataFrame
-  
-#  Returns:
-#  dataFrame 
-  
-#  bn-2016
-    
-# Making sure only to use the numeric columns 
-  y <- x[sapply(x,is.numeric)]
-    
-# We need at least two numeric columns to run the function
-  if(ncol(y)>=2) {
-
-# Possible combinations for each pair of column names (lists them in columns)
-    c <- combn(colnames(y), 2)
-
-# Creating the pairs by pasting the rows of c
-    pairs <- paste(c[1,], c[2,], sep = "-")
-
-# Creates the correlation coefficients between the columns of y as a matrix
-    m <- cor(y, method = "pearson")
-
-# We only need the entries from the lower or upper triangular, choose lower 
-# because their order corresponds to the strings created earlier. 
-# which() tells us the index values of the lower triangular andcorrespond 
-# to the order of the strings. m[] then outputs the values in that order
-    correlation <- m[which(lower.tri(m))]
-
-# Putting together the pieces for the new data frame
-    new_df <- data.frame(pairs, correlation)
-    return(new_df)
-  }
-  
-  else 
-    print(paste("This Data Frame does not have two or more numerical columns", 
-        "to compute the Pearson correlation coefficient(s)."))
-}  
-=======
-  ---
-  
-  corCoef <- function(x)
+corCoef <- function(x)
     #  This function corCoef() accepts any dataframe as a parameter and returns a
     #  dataframe that contains each pair of column names in the first column as a 
     #  single string separated by a "-" and their corresponding Pearson correlation
@@ -358,7 +223,7 @@ corCoef <- function(x){
     #  Returns:
     #  dataFrame 
     
-#  bn-2016
+    #  bn-2016
   {
     y <- x[sapply(x,is.numeric)]   # Making sure only to use the numeric columns
     
@@ -373,9 +238,9 @@ corCoef <- function(x){
       m <- cor(y, method = "pearson")   # Creates the correlation coefficients between the columns of y as a matrix
       
       correlation <- m[which(lower.tri(m))]  # We only need the entries from the lower or upper triangular, choose lower 
-      # because their order corresponds to the strings created earlier. 
-      # which() tells us the index values of the lower triangular andcorrespond 
-      # to the order of the strings. m[] then outputs the values in that order
+                                             # because their order corresponds to the strings created earlier. 
+                                             # which() tells us the index values of the lower triangular and correspond 
+                                             # to the order of the strings. m[] then outputs the values in that order
       
       new_df <- data.frame(pairs, correlation)   # Putting together the pieces for the new data frame
       return(new_df)
@@ -385,7 +250,6 @@ corCoef <- function(x){
       print(paste("This Data Frame does not have two or more numerical columns", 
                   "to compute the Pearson correlation coefficient(s)."))
   }  
->>>>>>> my-feature-branch
 
 # Feel free to test with various Data Frames
 corCoef(cars)
